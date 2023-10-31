aa51d4771e63eef63f126b7b0d1a3a4d5e174467
Ashish Sasanpuri <sashrao21@gmail.com>
2023-10-31 19:12:30 +0530

    Coped week 4 project and added Dockerfile

---
+++ Week 5/Scripts/app.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 24 14:08:45 2023
+
+@author: Ashish S
+"""
+
+import os
+import numpy as np
+import pickle as pkl
+from flask import Flask
+from flask import request 
+from flask import render_template
+
+app = Flask(__name__)
+
+# Load the model
+model = pkl.load(open('..\model.pkl', 'rb'))
+
+# Route to the homepage
+@app.route('/')
+def home():
+    
+    return render_template('home.html')
+
+# Prediction of inputs from the form
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    
+    features = [float(x) for x in request.form.values()]
+    features = [np.array(features)]
+    result = model.predict(features)[0]
+    result = round(result[0], 3)
+    
+    return render_template('home.html', prediction_result='Diabetes progression result is {}'.format(result))
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ Week 5/Scripts/model.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 23 17:42:18 2023
+
+@author: Ashish S
+"""
+
+# Importing Libraries
+import os
+import numpy as np
+import pandas as pd
+import pickle as pkl
+
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
+# Loading diabetes dataset
+diab_data = load_diabetes(as_frame=True)
+diab_df = pd.DataFrame(data=diab_data.data, 
+                       columns=diab_data.feature_names)
+
+diab_tar = pd.DataFrame(data=diab_data.target) 
+
+diab_df = diab_df.rename(columns={
+    's1' : 'tc',
+    's2' : 'ldl',
+    's3' : 'hdl',
+    's4' : 'tch',
+    's5' : 'ltg',
+    's6' : 'glu'
+    })
+
+data_df = pd.concat((diab_df, diab_tar), axis=1)
+
+print(data_df.head())
+
+# Splitting the data into train and test
+X_train, X_test, y_train, y_test = train_test_split(diab_df, 
+                                                    diab_tar, 
+                                                    test_size=0.2, 
+                                                    random_state=42)
+
+# Model training and prediction
+lr = LinearRegression()
+lr_model = lr.fit(X_train, y_train)
+y_pred = lr_model.predict(X_test)
+
+# Save the model
+pkl.dump(lr_model, open('../model.pkl', 'wb'))
+
+load_lr_model = pkl.load(open('../model.pkl', 'rb'))
+
+load_lr_model.predict([[0.0045341, 
+                        -0.044642, 
+                        -0.006206, 
+                        -0.015999, 
+                        0.125019, 
+                        0.125198, 
+                        0.019187, 
+                        0.034309, 
+                        0.032433, 
+                        -0.005220]])
+
